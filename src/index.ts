7ba8e6727dce3048143a4589b8d1dfd2c84a6e3d
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2023-03-26 11:26:35 -0300

    chore: init project 🎉

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const name = process.env.npm_package_name;
+const version = process.env.npm_package_version;
+const port = Number.parseInt(process.env.PORT || '3000', 10);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json('TBD');
+});
+
+app.get('/info', async (req, res) => {
+  res.send(`${name}@${version}`);
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
