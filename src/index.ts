038549ff672663d0f45e76142369b90b78e5c106
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2023-03-26 12:17:12 -0300

    chore: improving config

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const name = process.env.npm_package_name;
 const version = process.env.npm_package_version;
-const port = Number.parseInt(process.env.PORT || "3000", 10);
+const port = Number.parseInt(process.env.PORT || '3000', 10);
 
-app.get("/", (req: Request, res: Response) => {
-  res.json("TBD");
+app.get('/', (req: Request, res: Response) => {
+  res.json('TBD');
 });
 
-app.get("/info", async (req, res) => {
+app.get('/info', async (req, res) => {
   res.send(`${name}@${version}`);
 });
 
